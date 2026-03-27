834c7fc7566e3341c726dde858d67d3117ea7cc7
Charles Schlichenmeyer <charlesschlich@tamu.edu>
2026-03-27 10:03:26 -0500

    Implement createOrder endpoint and refactor getOrders in OrdersController

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Order } from './orders.entity';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
+    constructor(private readonly ordersService: OrdersService) {}
     @Get('')
-    getOrders() {
-        return 'Hello World';
+    async getOrders(): Promise<Order[]> {
+        return this.ordersService.getOrders();
+    }
+
+    @Post('')
+    async createOrder(order: Order): Promise<Order> {
+        return this.ordersService.createOrder(order);
     }
 }
