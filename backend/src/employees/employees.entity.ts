4b7abb0b06ad5c759beb8ea0294e2d05ea9cf8af
Kent Le <kentleisme@tamu.edu>
2026-03-27 16:59:44 -0500

    created employee database schema; has columns for id, name, role, wage, shift, and work status

---
+++ backend/src/employees/employees.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('employees')
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  shift: string;
+
+  @Column({ default: false })
+  isWorking: boolean;
+
+  @Column('decimal', { precision: 8, scale: 2 })
+  wage: number;
+}
