ba9f078c1187d3a2b71f0b93cab038233d26ffc1
mnguyen0512 <mnguyen0512@tamu.edu>
2026-03-28 17:57:06 -0500

    Modified page.tsx for menuboard

---
+++ frontend/app/menuboard/page.tsx
@@ -0,0 +1,20 @@
+import MenuScreen from "@/components/menuboard/MenuScreen";
+import { MenuItem } from "@/types/menuboard";
+
+export default async function MenuBoardPage() {
+  const res = await fetch("http://localhost:3001/menu-items", {
+    cache: "no-store", // ensures fresh data every time
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch menu items");
+  }
+
+  const items: MenuItem[] = await res.json();
+
+  return (
+    <div className="w-full h-full">
+      <MenuScreen items={items} />
+    </div>
+  );
+}
