1feafe2d187704759e2ccfd15285fd2a61cdd2a5
mnguyen0512 <mnguyen0512@tamu.edu>
2026-03-30 01:47:01 -0500

    Fix menuboard?

--- frontend/app/menu/page.tsx
+++ frontend/app/menu/page.tsx
@@ -1,23 +1,20 @@
+'use client';
+
 import MenuScreen from "@/components/menuboard/MenuScreen";
 import { MenuItem } from "@/types/menuboard";
+import { useEffect, useState } from "react";
+import { Get } from "@/utils/apiService";
 
-export default async function MenuBoardPage() {
-  //uncomment line below for local development
-  //const res = await fetch("http://localhost:3001/api/menu-items", {
+export default function MenuBoardPage() {
+  const [items, setItems] = useState<MenuItem[]>([]);
 
-  //Comment this line out for local development. Uncomment for production
-  const res = await fetch("https://project3-team-53-backend.vercel.app/api/menu-items", {
-    cache: "no-store", // ensures fresh data every time
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch menu items");
-  }
-
-  const items: MenuItem[] = await res.json();
+  useEffect(() => {
+    Get('/menu-items')
+      .then(setItems)
+      .catch(console.error);
+  }, []);
 
   return (
-    // The MenuScreen component is responsible for displaying the menu items and customization info.
     <div className="w-full h-full">
       <MenuScreen items={items} />
     </div>
