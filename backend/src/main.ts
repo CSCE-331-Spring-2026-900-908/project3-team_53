e952516307045671935beecfae210fd32001cec1
Charles Schlichenmeyer <charlesschlich@tamu.edu>
2026-03-30 09:02:49 -0500

    Update CORS configuration in main.ts to allow specific origins and enable credentials

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://project3-team-53.vercel.app'],
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3001);
   console.log(`Server is running on port ${process.env.PORT ?? 3001}`);
 }
